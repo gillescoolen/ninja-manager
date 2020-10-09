8b2081235c9efb0b97311a9a89fa494e5d9af903
Gilles <gilleswhc@gmail.com>
2020-10-09 13:46:37 +0200

    Fix missing key on many to many relation

--- NinjaManager.Domain/Data/DatabaseContext.cs
+++ NinjaManager.Domain/Data/DatabaseContext.cs
@@ -1,3 +1,4 @@
+using System;
 using Microsoft.EntityFrameworkCore;
 using NinjaManager.Domain.Models;
 
@@ -5,12 +6,35 @@ namespace NinjaManager.Domain.Data
 {
     public class DatabaseContext : DbContext
     {
-        public DatabaseContext (DbContextOptions<DatabaseContext> options)
+        public DatabaseContext(DbContextOptions<DatabaseContext> options)
             : base(options)
         {
         }
 
         public DbSet<Ninja> Ninja { get; set; }
         public DbSet<Gear> Gear { get; set; }
+        public DbSet<NinjaGear> NinjaGear { get; set; }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder
+                .Entity<Gear>()
+                .Property(e => e.Slot)
+                .HasConversion(
+                    t => t.ToString(),
+                    t => Enum.Parse<GearType>(t)
+                );
+
+            modelBuilder.Entity<Ninja>()
+                .HasIndex(u => u.Name)
+                .IsUnique();
+
+            modelBuilder.Entity<Gear>()
+                .HasIndex(u => u.Name)
+                .IsUnique();
+
+            modelBuilder.Entity<NinjaGear>()
+                .HasKey(t => new {t.GearId, t.NinjaId});
+        }
     }
 }
